a6af275a9773bf9bcf3083cddd136ec8b96f0685
Thomas Asadurian <Tasadurian@gmail.com>
2016-07-26 23:25:57 -0700

    added logic to catch json from POST

--- web/api.py
+++ web/api.py
@@ -15,6 +15,40 @@ def api():
         print(Sentiment.demo_vader_instance('This is a nice car.'))
         print(Sentiment.demo_vader_instance('This is a terrible car.'))
         return 'print'
+    if request.method == 'POST':
+        try:
+            f = request.get_json()
+        except Exception as e:
+            # Should log this error
+            print(e)
+            return not_acceptable()
+        else:
+            # Do stuff with json
+            print(f)
+            return 'hi'
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.errorhandler(406)
+def not_acceptable(error=None):
+    message = {
+            'status': 406,
+            'message': 'Could not parse json, check formatting:' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 406
+
+    return resp
 
 
 if __name__ == '__main__':
