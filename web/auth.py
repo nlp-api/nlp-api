3d3eb36a5cfc9886c9021df389f1bc335776461d
Thomas Asadurian <Tasadurian@gmail.com>
2016-08-05 23:50:00 -0700

    added simple redis auth

--- web/auth.py
+++ web/auth.py
@@ -1,13 +1,18 @@
 from functools import wraps
 from flask import g, request, redirect, url_for
 from errors import Errors
+import redis
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 class Auth():
     @staticmethod
     def auth_required(f):
         @wraps(f)
         def auth_function(*args, **kwargs):
-            if request.headers.get('auth') != 'nlp':
+            user = r.get(request.headers.get('auth'))
+            print(user)
+            if user == None:
                 return Errors.unauthorized()
             return f(*args, **kwargs)
         return auth_function
